df557a8ad58b563c378f1d1b65e386a885231b1b
wwwubhd <wwwredaktion@ub.uni-heidelberg.de>
2016-02-22 17:13:09 +0100

    Series subtitle

--- controllers/series.py
+++ controllers/series.py
@@ -20,7 +20,7 @@ def eva_info():
 def index():
   series = db((db.series.series_id==db.series_settings.series_id) & (db.series.press_id==myconf.take("omp.press_id")) & (db.series_settings.locale==locale)).select(db.series.path, db.series.image, db.series_settings.series_id, db.series_settings.setting_name, db.series_settings.setting_value, orderby= [db.series_settings.series_id, db.series_settings.setting_name] )
   series_metadata =[]
-  types = ['title','description']
+  types = ['title','subtitle','description']
   prev_series = 0
   for  row in series:
     if row['series_settings']['series_id'] != prev_series:
