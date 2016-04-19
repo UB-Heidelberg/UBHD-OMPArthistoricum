499aa8b49deea14bf3b253abe03701ba70177466
Katharina Wäschle <waeschle@ub.uni-heidelberg.de>
2016-04-19 11:13:15 +0200

    Download table with divs, format links

--- modules/customFormat.py
+++ modules/customFormat.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
 
+from html import URL
+
 def formatAuthor(author):
     return " ".join([author.first_name, author.middle_name, author.last_name])
 
 def formatAuthorCitationStyle(author):
     return "{}, {}".format(author.last_name, " ".join([author.first_name, author.middle_name]).strip())
+
+def downloadLink(application, file_row):
+    file_name_items = [file_row.submission_id,
+                       file_row.genre_id,
+                       file_row.file_id,
+                       file_row.revision,
+                       file_row.file_stage,
+                       file_row.date_uploaded.strftime('%Y%m%d'),
+                       file_row.file_type.split('/').pop().strip().lower()
+                       ]
+    file_name = '-'.join([str(i) for i in file_name_items])
+    return URL(application, 
+               'reader',
+               'download',
+               args=[file_row.submission_id, file_name]
+               )
