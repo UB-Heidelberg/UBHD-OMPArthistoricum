ac6f036aca5ebeefee9ee669b7cc7226fb9b449d
Katharina Wäschle <waeschle@ub.uni-heidelberg.de>
2016-04-19 11:44:18 +0200

    Small fixes, refactor format module, view option for html files

--- modules/customFormat.py
+++ modules/customFormat.py
@@ -2,24 +2,36 @@
 
 from html import URL
 
-def formatAuthor(author):
-    return " ".join([author.first_name, author.middle_name, author.last_name])
-
-def formatAuthorCitationStyle(author):
-    return "{}, {}".format(author.last_name, " ".join([author.first_name, author.middle_name]).strip())
+def formatAuthor(author_row, reverse=False):
+    """
+    Format author names for citations.
+    """
+    if not reverse:
+        return " ".join([author_row.first_name, author_row.middle_name, author_row.last_name])
+    else:
+        return "{}, {}".format(author_row.last_name, " ".join([author_row.first_name, author_row.middle_name]).strip())
 
 def downloadLink(application, file_row):
+    """
+    Generate download link from file info.
+    """
+    file_type = file_row.file_type.split('/').pop().strip().lower()
     file_name_items = [file_row.submission_id,
                        file_row.genre_id,
                        file_row.file_id,
                        file_row.revision,
                        file_row.file_stage,
                        file_row.date_uploaded.strftime('%Y%m%d'),
-                       file_row.file_type.split('/').pop().strip().lower()
                        ]
-    file_name = '-'.join([str(i) for i in file_name_items])
+    file_name = '-'.join([str(i) for i in file_name_items])+'.'+file_type
+    
+    if file_type == 'html':
+        op = 'index'
+    else:
+        op = 'download'
+    
     return URL(application, 
                'reader',
-               'download',
+               op,
                args=[file_row.submission_id, file_name]
                )
