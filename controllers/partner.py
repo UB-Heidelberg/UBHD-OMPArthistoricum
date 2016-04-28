7973e0b4d7b91cf73d3ed7ef74c8066c52c8f86a
Katharina Wäschle <waeschle@ub.uni-heidelberg.de>
2016-04-28 16:18:45 +0200

    Make partner site name a parameter instead of a controller

--- controllers/partner.py
+++ controllers/partner.py
@@ -5,15 +5,16 @@ Distributed under the GNU GPL v3. For full terms see the file
 LICENSE.md
 '''
 
+from os.path import exists
+
 def index():
-    return dict()
-
-def skd():
-    return dict()
-
-def ap():
-    return dict()
-
-def dbae():
-    return dict()
-
+    if request.args == []:
+        redirect( URL('home', 'index'))
+    partner_token = request.args[0]
+    
+    if exists(request.folder+'views/partner/'+partner_token+'.html'):
+        content = 'partner/'+partner_token+'.html'
+    else:
+        redirect( URL('home', 'index'))
+        
+    return locals()
