ed56621d5b2ca5e65f67fd34c23df77337c40544
Dulip Withanage <dulip.withanage@gmail.com>
2018-03-19 10:17:24 +0100

    https://gitlab.ub.uni-heidelberg.de/wit/verlag-portale/issues/114

--- controllers/home.py
+++ controllers/home.py
@@ -5,5 +5,9 @@ Distributed under the GNU GPL v3. For full terms see the file
 LICENSE.md
 '''
 
+from ompannouncements import Announcements
+
 def index():
-    return dict()
+    a = Announcements(myconf, db, locale)
+    news_list = a.create_announcement_list()
+    return locals()
