bf3c5322c93054da8594ae2fca4b246687d313e3
Dulip Withanage <dulip.withanage@gmail.com>
2018-05-14 09:30:25 +0200

    https://gitlab.ub.uni-heidelberg.de/wit/verlag-portale/issues/120

--- models/menu.py
+++ models/menu.py
@@ -9,3 +9,11 @@ response.subtitle = settings.subtitle
 response.meta.keywords = settings.keywords
 response.meta.description = settings.description
 response.menu = []
+
+if session.forced_language == 'en':
+    locale = 'en_US'
+elif session.forced_language == 'de':
+    locale = 'de_DE'
+else:
+    locale = 'de_DE'
+
