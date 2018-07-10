f278dcc1ceacc3d81dc7b3dafb3b9159518e2ff7
Dulip Withanage <dulip.withanage@gmail.com>
2018-07-10 13:29:34 +0200

    OMP 3.1 database changes https://gitlab.ub.uni-heidelberg.de/wit/verlag-portale/issues/186

--- models/tables_omp.py
+++ models/tables_omp.py
@@ -223,6 +223,7 @@ db.define_table("publication_formats",
                 Field("product_availability_code", "string"),
                 Field("technical_protection_code", "string"),
                 Field("returnable_indicator_code", "string"),
+                Field("remote_url", "string"),
                 Field("is_approved", "integer"),
                 Field("is_available", "integer"),
                 primarykey=['publication_format_id'],
@@ -301,18 +302,10 @@ db.define_table("submissions",
                 Field("series_position", "string"),
                 Field("edited_volume", "integer"),
                 Field("language", "string"),
-                Field("comments_to_ed", "string"),
+                #Field("comments_to_ed", "string"),
                 Field("date_submitted", "string"),
-                Field(
-                    "last_modified",
-                    "datetime",
-                    requires=IS_DATETIME(
-                        format="%Y-%m-%d %H:%M:%S")),
-                Field(
-                    "date_status_modified",
-                    "datetime",
-                    requires=IS_DATETIME(
-                        format="%Y-%m-%d %H:%M:%S")),
+                Field("last_modified","datetime",requires=IS_DATETIME(format="%Y-%m-%d %H:%M:%S")),
+                Field("date_status_modified","datetime",requires=IS_DATETIME(format="%Y-%m-%d %H:%M:%S")),
                 Field("status", "integer"),
                 Field("submission_progress", "integer"),
                 Field("pages", "string"),
@@ -366,7 +359,7 @@ db.define_table('submission_files',
                 Field('viewable', 'integer'),
                 Field('date_uploaded', 'datetime'),
                 Field('date_modified', 'datetime'),
-                Field('user_group_id', 'integer'),
+                #Field('user_group_id', 'integer'),
                 Field('uploader_user_id', 'integer'),
                 Field('assoc_type', 'integer'),
                 Field('assoc_id', 'integer'),
@@ -401,7 +394,7 @@ db.define_table("users",
                 Field("middle_name", "string"),
                 Field("last_name", "string"),
                 Field("suffix", "string"),
-                Field("gender", "string"),
+                #Field("gender", "string"),
                 Field("initials", "string"),
                 Field("email", "string"),
                 Field("url", "string"),
@@ -410,18 +403,10 @@ db.define_table("users",
                 Field("billing_address", "string"),
                 Field("country", "string"),
                 Field("locales", "string"),
-                Field("date_last_email",
-                      "datetime",
-                      requires=IS_DATETIME(format="%Y-%m-%d %H:%M:%S")),
-                Field("date_registered",
-                      "datetime",
-                      requires=IS_DATETIME(format="%Y-%m-%d %H:%M:%S")),
-                Field("date_validated",
-                      "datetime",
-                      requires=IS_DATETIME(format="%Y-%m-%d %H:%M:%S")),
-                Field("date_last_login",
-                      "datetime",
-                      requires=IS_DATETIME(format="%Y-%m-%d %H:%M:%S")),
+                Field("date_last_email","datetime",requires=IS_DATETIME(format="%Y-%m-%d %H:%M:%S")),
+                Field("date_registered","datetime",requires=IS_DATETIME(format="%Y-%m-%d %H:%M:%S")),
+                Field("date_validated","datetime",requires=IS_DATETIME(format="%Y-%m-%d %H:%M:%S")),
+                Field("date_last_login","datetime",requires=IS_DATETIME(format="%Y-%m-%d %H:%M:%S")),
                 Field("must_change_password", "integer"),
                 Field("auth_id", "integer"),
                 Field("auth_str", "string"),
@@ -444,37 +429,7 @@ db.define_table("user_settings",
                 migrate=False
                 )
 
-request_client = ''
-if request.client:
-    rcs = request.client.split('.')
-    if len(rcs) == 4:
-        request_client = rcs[0] + '.' + rcs[1] + '.' + rcs[2] + '.xxx'
 
-db.define_table('t_usage_statistics',
-                Field('time_stamp', 'datetime', default=request.now),
-                Field('client_ip', 'string', default=request_client),
-                Field(
-                    'request_controller',
-                    'string',
-                    default=request.controller),
-                Field('request_function', 'string', default=request.function),
-                Field(
-                    'request_extension',
-                    'string',
-                    default=request.extension),
-                Field('request_ajax', 'string', default=request.ajax),
-                Field('request_args', 'string', default=request.args),
-                Field('request_vars', 'string', default=request.vars),
-                Field('request_view', 'string', default=request.view),
-                Field('request_http_user_agent', 'string',
-                      default=request.env.http_user_agent),
-                Field(
-                    'request_language',
-                    'string',
-                    default=request.env.http_accept_language),
-                Field('description', 'text'),
-                migrate=False,
-                )
 
 db.define_table('t_license_settings',
                 Field( "license_id", "integer"),
@@ -484,3 +439,38 @@ db.define_table('t_license_settings',
                 migrate = False,
                 primarykey=["license_id" ,"locale", "setting_name"],
                 )
+
+
+#
+# request_client = ''
+# if request.client:
+#     rcs = request.client.split('.')
+#     if len(rcs) == 4:
+#         request_client = rcs[0] + '.' + rcs[1] + '.' + rcs[2] + '.xxx'
+#
+# db.define_table('t_usage_statistics',
+#                 Field('time_stamp', 'datetime', default=request.now),
+#                 Field('client_ip', 'string', default=request_client),
+#                 Field(
+#                     'request_controller',
+#                     'string',
+#                     default=request.controller),
+#                 Field('request_function', 'string', default=request.function),
+#                 Field(
+#                     'request_extension',
+#                     'string',
+#                     default=request.extension),
+#                 Field('request_ajax', 'string', default=request.ajax),
+#                 Field('request_args', 'string', default=request.args),
+#                 Field('request_vars', 'string', default=request.vars),
+#                 Field('request_view', 'string', default=request.view),
+#                 Field('request_http_user_agent', 'string',
+#                       default=request.env.http_user_agent),
+#                 Field(
+#                     'request_language',
+#                     'string',
+#                     default=request.env.http_accept_language),
+#                 Field('description', 'text'),
+#                 migrate=False,
+#                 )
+#
