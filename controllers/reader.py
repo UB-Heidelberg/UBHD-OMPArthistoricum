8647e28b8b8f05b6c5cfae805d7a701c05a0086f
Nils Weiher <weiher@ub.uni-heidelberg.de>
2019-07-18 11:26:06 +0200

    Fix catalog, tables, reader and update modules to work with OMP 3.1.2-1

--- controllers/reader.py
+++ controllers/reader.py
@@ -8,6 +8,9 @@ LICENSE.md
 # required - do no delete
 import os
 
+from ompdal import OMPDAL, OMPSettings, OMPItem
+import ompformat
+
 def user(): return dict(form=auth())
 # ef download(): return response.download(request,db)
 
@@ -17,6 +20,7 @@ def call():
 # end requires
 
 def index():
+    ompdal = OMPDAL(db, myconf)
     from gluon.serializers import json
     json_list = dict(xml_url='')
     locale = 'de_DE'
@@ -33,17 +37,11 @@ def index():
 
     # check if it is xml
     if str(file_id).endswith('.xml'):
-        query = ((db.submission_settings.submission_id == int(submission_id))
-                 & (db.submission_settings.locale == locale))
-        author_q = ((db.authors.submission_id == submission_id))
-        authors_list = db(author_q).select(
-            db.authors.first_name, db.authors.last_name)
-        authors = ''
-        for i in authors_list:
-            authors += i.first_name + ' ' + i.last_name + ', '
-        if authors.endswith(', '):
-            authors = authors[:-2]
-        return dict(json_list=XML(json(json_list)), authors=authors)
+        authors = [OMPItem(author, OMPSettings(ompdal.getAuthorSettings(author.author_id)))
+                   for author in ompdal.getAuthorsBySubmission(submission_id)]
+        authors_string = ', '.join((ompformat.formatName(a.settings) for a in authors))
+
+        return dict(json_list=XML(json(json_list)), authors=authors_string)
     else:
         path = os.path.join(request.folder, 'static/files/presses', myconf.take('omp.press_id'), 'monographs',
                             submission_id, 'submission/', file_id)
@@ -51,11 +49,6 @@ def index():
 
 def home():
     return dict()
-
-def index2():
-    from gluon.serializers import json
-    json_list = dict(xml_url='')
-    return dict(json_list=XML(json(json_list)))
 
 def download():
     submission_id = request.args[0]
